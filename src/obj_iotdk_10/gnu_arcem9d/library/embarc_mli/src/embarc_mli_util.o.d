4360ceec140a72efc7286a4f2187a9affd560cda
yuansung18 <35831896+yuansung18@users.noreply.github.com>
2019-08-18 21:02:47 +0800

    Add files via upload

---
+++ src/obj_iotdk_10/gnu_arcem9d/library/embarc_mli/src/embarc_mli_util.o.d
@@ -0,0 +1,4 @@
+obj_iotdk_10/gnu_arcem9d/library/embarc_mli/src/embarc_mli_util.o: \
+ ../../../../library/embarc_mli/src/embarc_mli_util.s \
+ ../../../../inc/arc/arc.h ../../../../inc/arc/arc_feature_config.h \
+ obj_iotdk_10/gnu_arcem9d/embARC_generated/core_config.h
