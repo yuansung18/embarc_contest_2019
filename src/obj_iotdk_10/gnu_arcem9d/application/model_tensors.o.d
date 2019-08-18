52ba931caae5e226468ce8b96d79d0d55bbcbdbd
yuansung18 <35831896+yuansung18@users.noreply.github.com>
2019-08-18 20:59:06 +0800

    Add files via upload

---
+++ src/obj_iotdk_10/gnu_arcem9d/application/model_tensors.o.d
@@ -0,0 +1,13 @@
+obj_iotdk_10/gnu_arcem9d/application/./model_tensors.o: model_tensors.c \
+ ../../../../library/embarc_mli/include/mli_api.h \
+ ../../../../library/embarc_mli/include/mli_types.h \
+ ../../../../library/embarc_mli/include/api/mli_helpers_api.h \
+ ../../../../library/embarc_mli/include/mli_types.h \
+ ../../../../library/embarc_mli/include/api/mli_kernels_api.h \
+ ../../../../library/embarc_mli/include/api/mli_krn_avepool_spec_api.h \
+ ../../../../library/embarc_mli/include/api/mli_krn_conv2d_spec_api.h \
+ ../../../../library/embarc_mli/include/api/mli_krn_depthwise_conv2d_spec_api.h \
+ ../../../../library/embarc_mli/include/api/mli_krn_maxpool_spec_api.h \
+ model_coeff.inc \
+ ../../../../library/embarc_mli/include/embarc_mli_sections.h \
+ ../../../../library/embarc_mli/include/mli_config.h
